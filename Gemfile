ee5d34889bded811d873e70b1b35f3bd655cd6cb
yamaguchiyudai <reosodo0102@gmail.com>
2018-10-10 14:36:50 +0000

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -62,5 +62,8 @@ group :production do
   gem 'pg', '0.20.0'
 end
 
+gem 'bootstrap-sass', '3.3.7'
+
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- test/helpers/application_helper.rb
+++ test/helpers/application_helper.rb
@@ -9,4 +9,15 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
+  
+  
+  
+  require 'test_helper'
+
+  class ApplicationHelperTest < ActionView::TestCase
+    test "full title helper" do
+      assert_equal full_title,         FILL_IN
+      assert_equal full_title("Help"), FILL_IN
+    end
+  end
 end
